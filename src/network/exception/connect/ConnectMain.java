5237df5668b22966da2cd0ba1b78493a6064a51c
ghdcksgml1 <ghdcksgml2@naver.com>
2025-02-03 13:39:59 +0000

    feat: 네트워크 - 프로그램2 완료!

---
+++ src/network/exception/connect/ConnectMain.java
@@ -0,0 +1,42 @@
+package network.exception.connect;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ConnectMain {
+    public static void main(String[] args) {
+        unknownHostEx1();
+        unknownHostEx2();
+        connectionRefused();
+    }
+
+    private static void unknownHostEx1() {
+        try {
+            Socket socket = new Socket("999.999.999.999", 80);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void unknownHostEx2() {
+        try {
+            Socket socket = new Socket("google.gogo", 80);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void connectionRefused() {
+        try {
+            Socket socket = new Socket("localhost", 45678);
+        } catch (ConnectException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
